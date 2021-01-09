10f9bbb7a33420723e10d9b706731e3a4791975c
Burbuha <burbovskaja@gmail.com>
2021-01-10 01:12:17 +0300

    feat: implement the createDreamTeam(members) function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secretName = [];
+  if (!Array.isArray(members)) return false;
+  members.forEach((member) => {
+    if (typeof member == "string") {
+      secretName.push(member.trim()[0].toUpperCase());
+    }
+    return secretName;
+  });
+  return secretName.length ? secretName.sort().join("") : false;
 };
